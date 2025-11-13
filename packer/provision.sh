d17c39fed96ed1394a63185b37e13d2cd82315eb
fasil <ec2-user@ip-172-31-12-13.ap-northeast-1.compute.internal>
2025-11-13 21:36:50 +0000

    3nd Updated Version 1

--- packer/provision.sh
+++ packer/provision.sh
@@ -4,6 +4,6 @@ yum install -y httpd php
 systemctl restart httpd.service php-fpm.service
 systemctl enable httpd.service php-fpm.service
 cp -r /tmp/website/* /var/www/html
-chown -R apache:apache /var/www/html
+chown -R apache:apache /var/www/html/
 rm -rf /tmp/website
 
